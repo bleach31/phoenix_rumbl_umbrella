b34375e86f7a928652f8d6591eb3ae2c4d85c67f
Motohiro <c201075@gmail.com>
2023-09-06 10:31:30 +0000

    266のXMLの手間まで

--- apps/info_sys/lib/info_sys.ex
+++ apps/info_sys/lib/info_sys.ex
@@ -1,18 +1,30 @@
+#---
+# Excerpted from "Programming Phoenix 1.4",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/phoenix14 for more book information.
+#---
 defmodule InfoSys do
-  @moduledoc """
-  Documentation for InfoSys.
-  """
 
-  @doc """
-  Hello world.
+  @backends [InfoSys.Wolfram]
 
-  ## Examples
+  defmodule Result do
+    defstruct score: 0, text: nil, backend: nil
+  end
 
-      iex> InfoSys.hello()
-      :world
+  def compute(query, opts \\ []) do
+    opts = Keyword.put_new(opts, :limit, 10)
+    backends = opts[:backends] || @backends
 
-  """
-  def hello do
-    :world
+    backends
+    |> Enum.map(&async_query(&1, query, opts))
+  end
+
+  defp async_query(backend, query, opts) do
+    Task.Supervisor.async_nolink(InfoSys.TaskSupervisor,
+      backend, :compute, [query, opts], shutdown: :brutal_kill
+    )
   end
 end
