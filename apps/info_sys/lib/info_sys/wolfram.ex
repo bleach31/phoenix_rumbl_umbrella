b8b70cb7adbee807692d0feff52e98e4be3282c8
Motohiro <c201075@gmail.com>
2023-11-29 10:36:36 +0000

    add wolfram backend test

--- apps/info_sys/lib/info_sys/wolfram.ex
+++ apps/info_sys/lib/info_sys/wolfram.ex
@@ -10,7 +10,7 @@ defmodule InfoSys.Wolfram do
   import SweetXml
   alias InfoSys.Result
 
-  @behaviour InfoSys.Backend 
+  @behaviour InfoSys.Backend
 
   @base "http://api.wolframalpha.com/v2/query"
 
@@ -18,7 +18,7 @@ defmodule InfoSys.Wolfram do
   def name, do: "wolfram"
 
   @impl true
-  def compute(query_str, _opts) do 
+  def compute(query_str, _opts) do
     query_str
     |> fetch_xml()
     |> xpath(~x"/queryresult/pod[contains(@title, 'Result') or
@@ -27,20 +27,21 @@ defmodule InfoSys.Wolfram do
     |> build_results()
   end
 
-  defp build_results(nil), do: [] 
+  defp build_results(nil), do: []
 
   defp build_results(answer) do
     [%Result{backend: __MODULE__, score: 95, text: to_string(answer)}]
   end
 
-  defp fetch_xml(query) do 
-    {:ok, {_, _, body}} = :httpc.request(String.to_charlist(url(query)))
+  @http Application.get_env(:info_sys, :wolfram)[:http_client] || :httpc
+  defp fetch_xml(query) do
+    {:ok, {_, _, body}} = @http.request(String.to_charlist(url(query)))
 
     body
   end
 
   defp url(input) do
-    "#{@base}?" <> 
+    "#{@base}?" <>
     URI.encode_query(appid: id(), input: input, format: "plaintext")
   end
 
--- config/test.exs
+++ config/test.exs
@@ -19,3 +19,8 @@ config :logger, level: :warn
 
 # faster hashing
 config :pbkdf2_elixir, :rounds, 1
+
+# test info_sys walfram backend
+config :info_sys, :wolfram,
+  app_id: "1234",
+  http_client: InfoSys.Test.HTTPClient
